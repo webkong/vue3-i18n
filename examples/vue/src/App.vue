cd1098b2b6a110429d2bd165cd15cb3771ee0eb3
webkong <772930508@qq.com>
2021-10-20 10:38:22 +0800

    Fix #7 #5 not found bug

--- examples/vue/src/App.vue
+++ examples/vue/src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <hello-word msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
-
+console.log(HelloWorld);
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    "hello-word": HelloWorld,
   },
 };
 </script>
